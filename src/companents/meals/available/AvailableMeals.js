ab3a4e3b85c1f1345ec2802f26863141eaa010e3
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-08 01:02:19 +0300

    adding card component

--- src/companents/meals/available/AvailableMeals.js
+++ src/companents/meals/available/AvailableMeals.js
@@ -1,4 +1,5 @@
 import classes from "./AvailableMeals.module.css";
+import Card from "../../UI/card/Card";
 
 const DUMMY_MEALS = [
   {
@@ -32,7 +33,9 @@ const AvailableMeals = () => {
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
